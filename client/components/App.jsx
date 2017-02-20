a8390dfaeaa450d67480d6820c2d5386be3fd933
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-20 15:23:24 +0530

    Initial Commit

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render () {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+        <div>My first react app</div>
+      </div>
+    )
+  }
+}
